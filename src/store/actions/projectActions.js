b0ebab1e6e96d4486fb42714484da8da5bafa6c0
miyass <exe.miyakura0629@gmail.com>
2018-10-12 17:47:21 +0900

    ver18

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,6 +2,18 @@
 export const createProject = (project) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to database
-    dispatch({ type: 'CREATE_PROJECT', project: project });
+    const firestore = getFirestore();
+    console.log(project);
+    firestore.collection('projects').add({
+      ...project,
+      authorFirstName: 'Ryu',
+      authorLastName: 'Hogehoge',
+      authorId: 12345,
+      createdAt: new Date()
+    }).then(() => {
+      dispatch({ type: 'CREATE_PROJECT', project: project });
+    }).catch((error) => {
+      dispatch({ type: 'CREATE_PROJECT_ERROR', error });
+    })
   }
 };
